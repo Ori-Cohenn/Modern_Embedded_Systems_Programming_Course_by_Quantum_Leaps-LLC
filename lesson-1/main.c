0db471d28fdefc8e08c874ae3f0c1680213caec9
Ori-Cohenn <Oricoh100@gmail.com>
2024-06-14 10:42:57 +0300

    initial commit!

---
+++ lesson-1/main.c
@@ -0,0 +1,12 @@
+// the purpose of this lesson is viewing the disassembly, memory and regs 
+int main(){
+	int counter= 0;
+	++counter;
+	++counter;
+		++counter;
+		++counter;
+		++counter;
+		++counter;
+		++counter;
+return 0;
+}
